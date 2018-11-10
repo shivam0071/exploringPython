407ef38c91e9f809e5efddd56f0944bbc6517e86
Shivam <shivam0071@users.noreply.github.com>
2018-11-10 21:50:46 +0530

    Strings and List Interview

--- Interview/String_Ques.py
+++ Interview/String_Ques.py
@@ -1,7 +1,66 @@
+# Basic and Easy Questions
 
-# 1.) Ques - You need to split a string into fields, but the delimiters
+# 1) WAP of reverse a str1ing
+
+str1 = 'A string That Need TO Be Reversed'
+print(str1[::-1])
+
+# 2) WAP to split the string a store in a list
+print(str1.split())
+
+# 3) WAP to split the string to store only even items
+print(str1.split()[::2])
+# the thing we learn here is no water how many index you want to jump it always starts with index 0
+print(str1.split()[1::2])
+# Even for a human
+
+# 4.) WAP to lower the string
+print(str1.lower())
+
+# 5.) Swap the cases in the string
+# eg - EthanS to eTHANs
+print(str1.swapcase())
+# Impressive
+
+# 6.) WAP to check if a substring is available in the string
+print(str1.find('ver'))
+# so it indeed search for a substring...cool
+
+# 7.)  WAP to capitalize the string
+print(str1.capitalize())
+
+# 8 WAP to add 3 '*' on both side of the string
+str1_len = len(str1)
+print(str1_len)
+print(str1.center(str1_len+6,'*'))
+        # OR
+print(format(str1, '*^'+str(str1_len+6)))
+
+#9 ) WAP to hide first 12 digits of a 16 digit account no
+account_no = '2233773456788899'
+print('*'*12+''+account_no[len(account_no)-4:])
+
+# 10 WAP to remove the traling whitespaces...after that remove the leading white spaces and the both
+
+str2 = '      HELLO     WORLD          '
+print(str2)
+print(str2.lstrip().rstrip())
+print(str2.strip())
+
+
+#  11 WAP to remove all the vowels
+
+vowels = ['a','e','i','o','u']
+print(str1)
+print (''.join([ch for ch in str1 if ch.lower() not in vowels]).strip())
+
+# WAP to delete a range of strings
+n=10
+print(str1[n:])
+# 1.) Ques - You need to split a str1ng into fields, but the delimiters
 # (and spacing around them) aren’t
-# consistent throughout the string.
+# consistent throughout the str1ing.
+
 
 line = 'asdf fjdk; afed, fjek,asdf, foo'
 import re
---
+++ Interview/list_ques.py
@@ -0,0 +1,59 @@
+#1 WAP to get 2 highest no in a list
+ls = [33, 44, 55, 11, 99, 22, 1, 34, 56, 78, 98]
+
+print(sorted(ls)[-2:])
+
+#2 WAP to convert list into DICT
+
+print (dict((key, value) for key, value in enumerate(ls)))
+
+# 3 WAP to flatten a list
+
+ls1 = [1, 2, 3, 4 , [5, 6, 7], 8 ,9, 10, [11, 23 , [33,77, [90, 55,12,334]]], 69, 6969]
+print(ls1)
+ls2 = []
+
+def flatten(ls):
+  for item in ls:
+    if isinstance(item, list):
+      flatten(item)
+      continue
+    ls2.append(item)
+flatten(ls1)
+print(ls2)
+
+#4 WAP to merge 2 dict
+dic1 = {'Name': 'Shaan', 'Position': 'Senior Singer'}
+dic2 = {'Address':'Nowhere Town', 'Pin': '11223344'}
+# print(dict(zip(dic1,dic2)))
+# print(dic1.update(dic2))
+merged = dic1.copy()#.update(dic2)
+merged.update(dic2)
+print(merged)
+
+#5 print all keys with vowel as a value
+dic3 = {1:'a',2:'c',3:'e'}
+vowels = ['a','e','i','o','u']
+print([key for key, value in dic3.items() if value in vowels])
+
+#6 WAP to empty a dictionary
+
+dic3.clear()
+print(dic3)
+
+
+#7 wap to get common elements in a list/tuple
+
+ls1 = [1,2,4,44,3,2,1]
+ls2 = [44,3,111,99,44]
+print(set(ls1).intersection(ls2))
+
+# 8  Different Elements
+print(set(ls1) ^ set(ls2))
+
+# shift all the zeros in a list to RHS
+
+ls4 = [1,2,0,44,0,66,0,5644,3,0,33,4]
+ls5 = [item for item in ls4 if item != 0]
+ls5.extend([0] * (len(ls4) - len(ls5)))
+print(ls5)
