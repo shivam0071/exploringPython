30728014bc4c830e9ae7e9f7fd4338c4ce5c8bd8
Shivam <shivam0071@users.noreply.github.com>
2018-12-02 19:13:48 +0530

    process vs Thread

--- Python_2018/abstractclassDemo.py
+++ Python_2018/abstractclassDemo.py
@@ -5,7 +5,7 @@ class AbstractClassExample(ABC):
 
   def __init__(self, value):
     self.value = value
-    super().__init__()
+    # super().__init__()
 
   @abstractmethod
   def do_something(self):
--- Python_2018/threadVSprocess.py
+++ Python_2018/threadVSprocess.py
@@ -8,16 +8,16 @@ def task(n):
     n -= 1
 
 
-def time_taken(fun):
-  # print ('Running '+name)
-  print(th.active_count())
-  print(th.current_thread())
-  def wrapper(n):
-    start = time.time()
-    fun(n)
-    end = time.time()
-    # print('Time taken by '+name,end-start)
-  return wrapper
+def time_taken(name):
+  def wrap(fun):
+    def wrapper(n):
+      print('Running', name)
+      start = time.time()
+      fun(n)
+      end = time.time()
+      print('Time taken by '+name,end-start)
+    return wrapper
+  return wrap
 
 # def threading_info(func):
 #   # def wrapper(w):
@@ -25,18 +25,43 @@ def time_taken(fun):
 #     return lambda x: print('Heya',x)
 #   #   return func
 
-@time_taken
+@time_taken('Thread')
 def threading_pace(n):
-  print('Running a thread')
   t1 = Thread(target=task,args=(n,))
   t2 = Thread(target=task,args=(n,))
+  t3 = Thread(target=task, args=(n,))
+  t4 = Thread(target=task, args=(n,))
   t1.start()
   t2.start()
+  t3.start()
+  t4.start()
   t1.join()
   t2.join()
+  t3.join()
+  t4.join()
+
+@time_taken('Process')
+def process_pace(n):
+  p1 = Process(target=task,args=(n,))
+  p2 = Process(target=task,args=(n,))
+  p3 = Process(target=task,args=(n,))
+  p4 = Process(target=task,args=(n,))
+
+  p1.start()
+  p2.start()
+  p3.start()
+  p4.start()
+  p1.join()
+  p2.join()
+  p3.join()
+  p4.join()
+
+
 
 if __name__ == '__main__':
-  num = 5000000
+  num = 50000000
   threading_pace(num)
+  process_pace(num)
   print('MAIN')
+
   # print(threading_info())
