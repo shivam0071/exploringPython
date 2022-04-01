ace9646e59ba20068e383704430efe4946833090
shivam0071 <shivamkoundal01@gmail.com>
2022-04-01 11:43:38 +0530

    dict

--- dictionaries_basics.py
+++ dictionaries_basics.py
@@ -21,3 +21,43 @@ student_tuples = [
 
 from operator import itemgetter, attrgetter
 print(sorted(student_tuples, key=itemgetter(2)))
+
+import collections
+def sort_this(ls):
+    print(ls)
+    res = []
+    left_idx = 0
+    for idx, data in enumerate(ls[:]):
+        if data < 0:
+            ls.insert(left_idx, ls.pop(idx))
+            left_idx += 1
+        elif data > 0:
+            ls.append(data)
+            res.append(data)
+    else:
+        for d in res:
+            ls.remove(d)
+    print(ls)
+
+def sort_this_2_pointers(ls):
+    print(ls)
+    left = 0
+    right = len(ls) - 1
+    idx = 0
+    while left < right:
+        data = ls[left]
+        if data < 0:
+            ls[idx], ls[left] = ls[left], data
+            left += 1
+        elif data > 0:
+            ls[idx], ls[right] = ls[right], data
+            right -= 1
+        else:
+            left += 1
+            idx += 1
+
+    print(ls)
+
+# sort_this([10, 4, -1, -4, 0, 6, 0, -8, -9])
+
+sort_this_2_pointers([10, 4, -1, -4, 0, 6, 0, -8, -9])
